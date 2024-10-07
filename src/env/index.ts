770611df423d3df7d646337a383af67223da1170
Castro <103647024+joaoguicastro@users.noreply.github.com>
2024-10-07 16:48:48 -0300

    a

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 // Definindo o esquema de validação para as variáveis de ambiente
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  PORT: z.coerce.number().default(4000),
+  PORT: z.coerce.number().default(3333),
   JWT_SECRET: z.string().min(1, "JWT_SECRET is required"), // Verifica que a string não está vazia
   DATABASE_URL: z.string().url().optional(), // Exemplo para garantir que a variável de conexão do banco seja uma URL válida (opcional)
 });
