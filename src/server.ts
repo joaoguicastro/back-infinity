120b73f1abb345565cf2fe1af357ffde00b7db83
Castro <103647024+joaoguicastro@users.noreply.github.com>
2024-10-07 16:05:53 -0300

    a

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ server.register(authRoutes);
 server.register(financeiroRoutes);
 server.register(usuarioRoutes);
 
-server.listen({ port: env.PORT, host: '0.0.0.0' }, (err, address) => {
+server.listen({ port: 4000, host: '0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
